091a7c40bb823407d18029e6a372a2030b968645
Schwalbe Sophie <sophie@codeup.berlin>
2020-03-06 22:08:13 +0100

    basic resource verification added

---
+++ src/Resource.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @param $value
+ * @param string $expectedResourceType
+ * @return bool
+ */
+function is_resource_of_type($value, string $expectedResourceType): bool
+{
+    return is_resource($value) && $expectedResourceType === get_resource_type($value);
+}
+
+/**
+ * @param $value
+ * @param string $expectedResourceType
+ * @throws \UnexpectedValueException
+ */
+function assert_resource_of_type($value, string $expectedResourceType): void
+{
+    if (!is_resource($value)) {
+        throw new UnexpectedValueException('Not a resource.');
+    }
+    $resourceType = get_resource_type($value);
+    if ($expectedResourceType !== $resourceType) {
+        throw new UnexpectedValueException(
+            "Resource of type \"$resourceType\" is not of type \"$expectedResourceType\"."
+        );
+    }
+}
---
+++ src/ResourceType.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @see https://www.php.net/manual/de/resource.php
+ */
+const RESOURCE_TYPE_STREAM = 'stream';
+
+/**
+ * @see https://www.php.net/book.zip
+ */
+const RESOURCE_TYPE_ZIP_DIRECTORY = 'Zip Directory';
+const RESOURCE_TYPE_ZIP_ENTRY = 'Zip Entry';
